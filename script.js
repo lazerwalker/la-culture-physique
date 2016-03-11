5715956f3227e4494683577b247729502e14562c
Mike Walker <michael@lazerwalker.com>
2016-03-11 11:32:03 -0700

    Count exercises

--- script.js
+++ script.js
@@ -1,13 +1,43 @@
+var bottomTimeout, topTimeout;
+var reachedBotton = false;
+var reachedTop = false;
+var count = 0;
+
+checkState = function() {
+  if (reachedBottom && reachedTop) {
+    count++;
+    console.log("Completed " + count + " exercises")
+
+    reachedTop = false;
+    reachedBottom = false;
+  }
+}
+
+
 $(document).on('touchstart mousedown keydown', function(e) {
   if(e.type === "keydown" && e.keyCode !== 32) {
-    return
+    return;
   }
-  $(".leg").addClass('down')
+  $(".leg").addClass('down');
+
+  // TODO: Try to listen for CSS animation end
+  clearTimeout(topTimeout)
+  bottomTimeout = setTimeout(function() {
+    reachedBottom = true;
+  }, 500)
 })
 
 $(document).on('touchend mouseup keyup', function(e) {
   if(e.type === "keyup" && e.keyCode !== 32) {
-    return
+    return;
   }
-  $(".leg").removeClass('down')
+
+  clearTimeout(bottomTimeout);
+  topTimeout = setTimeout(function() {
+    reachedTop = true;
+    checkState();
+  }, 500)
+
+  $(".leg").removeClass('down');
 })
+
