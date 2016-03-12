09ba74083a26f56a953dfc6dd1e98f63653252f6
Mike Walker <michael@lazerwalker.com>
2016-03-12 11:49:10 -0800

    Add messages

--- script.js
+++ script.js
@@ -42,6 +42,63 @@ function endTap(e) {
   $(".leg").addClass('down');
 }
 
+function addMessage(msg) {
+  console.log("Showing " + msg)
+
+  $("#game .msg")
+    .hide()
+    .css({
+      left: Math.random() * 60 + 20,
+      top: Math.random() * 300,
+      "font-size": 20 + Math.random() * 20,
+    })
+    .html(msg)
+    .fadeIn(1000)
+}
+
+function startPlayingMessages() {
+  console.log("Starting to play")
+  var messages = _.shuffle([
+    "Ahhhhh",
+    "Trés bien!",
+    "Trés relaxant!",
+    "Vous êtes magnifique!",
+    "Marveleux!",
+    "La vie est belle",
+    "Parfait!",
+    "Je vous aime",
+    "Super!",
+    "Mmmm"
+  ])
+
+  var usedMessages = []
+
+  function iter() {
+    console.log("In iter")
+    if(Math.random() > 0.8) {
+      if (messages.length === 0) {
+        messages = usedMessages;
+        usedMessages = [];
+      }
+
+      var msg = _.sample(messages)
+      addMessage(msg)
+      usedMessages.push(msg)
+      messages = _(messages).without(msg)
+
+      setTimeout(function() {
+        $("#game .msg").fadeOut(100)
+        setTimeout(iter, 500)
+      }, 1300)
+    } else {
+      console.log("Didn't make it")
+      setTimeout(iter, 100)
+    }
+  }
+
+  setTimeout(iter, 1000)
+}
+
 $(document).on('touchstart mousedown keydown', startTap);
 $(document).on('touchend mouseup keyup', endTap);
 
